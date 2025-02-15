d62f4d005aa3199defa9ff576b63173e79018911
devendramishra17 <devendramishra17@gmail.com>
2025-02-15 11:17:35 +0530

    Here in this commit we have implemented DTO successfully

--- src/main/java/com/javaguides/springboot_restful_webservices/service/UserService.java
+++ src/main/java/com/javaguides/springboot_restful_webservices/service/UserService.java
@@ -1,18 +1,19 @@
 package com.javaguides.springboot_restful_webservices.service;
 
 
+import com.javaguides.springboot_restful_webservices.dto.UserDto;
 import com.javaguides.springboot_restful_webservices.entity.User;
 
 import java.util.List;
 
 public interface UserService {
-    User createUser(User user);
+    UserDto createUser(UserDto user);
 
-    User getUserById(Long userId);
+    UserDto getUserById(Long userId);
 
-    List<User> getAllUsers();
+    List<UserDto> getAllUsers();
 
-    User updateUser(User user);
+    UserDto updateUser(UserDto user);
 
     void deleteUser(Long userId);
 }
