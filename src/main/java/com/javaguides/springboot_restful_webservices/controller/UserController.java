7a357b3575a7a68b0d5a942ad5e9387f769962fb
devendramishra17 <devendramishra17@gmail.com>
2025-02-17 17:25:14 +0530

    In this update I have updated the application's REST API validation.

--- src/main/java/com/javaguides/springboot_restful_webservices/controller/UserController.java
+++ src/main/java/com/javaguides/springboot_restful_webservices/controller/UserController.java
@@ -5,6 +5,7 @@ import com.javaguides.springboot_restful_webservices.entity.User;
 import com.javaguides.springboot_restful_webservices.exception.ErrorDetails;
 import com.javaguides.springboot_restful_webservices.exception.ResourceNotFoundException;
 import com.javaguides.springboot_restful_webservices.service.UserService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +25,7 @@ public class UserController {
 
     // build create User REST API
     @PostMapping
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto user){
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto user){
         UserDto savedUser = userService.createUser(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
@@ -49,7 +50,7 @@ public class UserController {
     @PutMapping("{id}")
     // http://localhost:8080/api/users/1
     public ResponseEntity<UserDto> updateUser(@PathVariable("id") Long userId,
-                                           @RequestBody UserDto user){
+                                           @RequestBody @Valid UserDto user){
         user.setId(userId);
         UserDto updatedUser = userService.updateUser(user);
         return new ResponseEntity<>(updatedUser, HttpStatus.OK);
